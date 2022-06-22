6a2e4725bc0a55addc8aae7179c7061cbaf3bb71
kubakosmal <44708470+kubakosmal@users.noreply.github.com>
2022-06-22 19:07:39 +0200

    fix: fix guest playlists

--- pages/api/guest.ts
+++ pages/api/guest.ts
@@ -1,31 +1,55 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import prisma from '../../lib/prisma'
+import { Song } from '../../types/data'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const playlists = await prisma.playlist.findMany({
+  let playlists = new Array()
+
+  playlists[0] = await prisma.playlist.findFirst({
     where: {
-      OR: [
-        { name: 'Gym' },
-        { name: 'Go Vote' },
-        { name: 'The best of ice cream truck' },
-      ],
+      name: 'Gym',
+    },
+    include: {
+      songs: true,
     },
   })
-  const playlistsIds = playlists.map((playlist) => {
-    return {
-      id: playlist.id,
-    }
+  playlists[1] = await prisma.playlist.findFirst({
+    where: {
+      name: 'Go Vote',
+    },
   })
+  playlists[2] = await prisma.playlist.findFirst({
+    where: {
+      name: 'The best of ice cream truck',
+    },
+  })
+
   const user = await prisma.user.create({
     data: {
       name: 'Joe Biden',
       image:
         'https://s.abcnews.com/images/Politics/biden-file-gty-ml-201106_1604680381500_hpMain_16x9_1600.jpg',
-      playlists: {
-        connect: playlistsIds,
-      },
     },
   })
 
+  const songsIds = playlists[0].songs.map((song: Song) => ({ id: song.id }))
+
+  const newPlaylists = playlists.map(async (playlist) => {
+    const newPlaylist = await prisma.playlist.create({
+      data: {
+        name: playlist.name,
+        image: playlist.image,
+        user: {
+          connect: {
+            id: user.id,
+          },
+        },
+        songs: {
+          connect: songsIds,
+        },
+      },
+    })
+    return newPlaylist
+  })
   res.json(user)
 }
